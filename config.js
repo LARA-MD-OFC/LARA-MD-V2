aa4e0461650c40402c3f5d34310c10875cb9c575
NOVA-MD-OFC <laravofficialbot@gmail.com>
2025-07-28 14:48:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "4FYiEA5Y#OFa-yn-p0CPQpmnanXpQLfjkuRIrxlJT-xUtHWNX474",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:mAnUPgNyLGcMHjTMiwFcpviLpvbOiamI@crossover.proxy.rlwy.net:36230",
+  OWNER_NUM: process.env.OWNER_NUM || "94778080044",
 };
